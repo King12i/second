7db87d5347c53c92c52360f7d1e704f62561f746
Lutteur Ngengwa <ngengwalutteur11@gmail.com>
2020-02-17 16:58:48 -0800

    This is my work

---
+++ first.js
@@ -0,0 +1,10 @@
+var num1 = 6;
+var num2 = 12;
+var mult = num1*2;
+if (mult==num2)
+{
+    console.log(num2)
+}
+else {
+    console.log(mult)
+}
---
+++ second.js
@@ -0,0 +1,14 @@
+var sam =36;
+var shem = 25;
+var lut = 18;
+if (sam > shem )
+{
+    console.log("sam is old")
+}
+else if(shem < lut)
+{
+    console.log("shem is big")
+}
+else {
+    console.log("lut is young")
+}
